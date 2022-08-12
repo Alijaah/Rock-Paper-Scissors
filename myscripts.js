b6750bf3d1ba7085ad3de45cb0662a78593b1d2a
Aidan Dyer <aidan.dyer@live.ca>
2022-08-12 12:25:19 -0400

    add functionality

--- myscripts.js
+++ myscripts.js
@@ -3,25 +3,83 @@
 const choices = ["rock", "paper", "scissors"];
 let winners = [];
 
-// create function that plays until 5 rounds
+// create function that resets the game
 
-function game() {
-    playRound(i);
-  setWins();  
+function resetGame() {
+
 }
 
-// functin that plays a round
+// function that plays the game until someone wins 5 times
 
-function playRound(round) {
+function startGame() {
+  let imgs = document.querySelectorAll('img')
+  imgs.forEach((img) => 
+  img.addEventListener(('click'), () => {
+    if (img.id){
+      playRound(img.id);
+  }})) 
+}
+
+// function that plays a round
+
+function playRound(playerChoice) {
+  let wins = checkWins();
+    if (wins >= 5) {
+      return;
+    }
+
   const computerChoice = computerSelection();
   const winner = checkWinner(playerChoice, computerChoice);
   winners.push(winner);
+  tallyWins();
+  displayRound(playerChoice, computerChoice, winner);
+  wins = checkWins();
+  if (wins == 5) {
+      displayEnd()
+    }
+}
+
+function displayEnd() {
+  let playerWins = winners.filter((item) => item == "Player").length;
+
+  if (playerWins == 5) {
+    document.querySelector('winner').textContent = 'You won 5 games! Congratulations!';
+  } else {
+    document.querySelector('winner').textContent = 'Sorry, the computer won 5 times.';
+  }
+  document.querySelector('reset').style.display = 'flex';
+}
+
+function displayRound(playerChoice, computerChoice, winner) {
+  document.querySelector('.playerChoice').textContent = `You chose ${playerChoice.charAt(0).toUpperCase().slice(1)}`;
+  document.querySelector('.computerChoice').textContent = `The Computer chose ${playerChoice.charAt(0).toUpperCase().slice(1)}`;
+  document.querySelector('winner').textContent = `Round Winner: ${winner}`;
+}
+
+// function that tallys wins  
+
+function tallyWins() {
+  const pWinCount = winners.filter((item) => item == "Player").length;
+  const cWinCount = winners.filter((item) => item == "Computer").length;
+  const ties = winners.filter((item) => item == "Tie").length;
+  document.querySelector('.playerScore').textContent = `Score ${pWinCount}`;
+  document.querySelector('.computerScore').textContent = `Score ${cWinCount}`;
+  document.querySelector('.ties').textContent = `Score ${ties}`;
+
 }
 
 // function that returns a random computer choice
 
 function computerSelection() {
   return choices[Math.floor(Math.random() * choices.length)];
+}
+
+//
+
+function checkWins() {
+  const pWinCount = winners.filter((item) => item == "Player").length;
+  const cWinCount = winners.filter((item) => item == "Computer").length;
+  return Math.max(pWinCount, cWinCount);
 }
 
 // function that checks who won
@@ -42,8 +100,10 @@ function checkWinner(choiceP, choiceC) {
 
 // function that logs the amount of wins
 
-function logWins() {
-  let playerWins = winners.filter((item) => item == "Player").length;
-  let computerWins = winners.filter((item) => item == "Computer").length;
-  let ties = winners.filter((item) => item == "Tie").length;
+function setWins() {
+  const pWinCount = winners.filter((item) => item == "Player").length;
+  const cWinCount = winners.filter((item) => item == "Computer").length;
+  const ties = winners.filter((item) => item == "Tie").length;
 }
+
+startGame();  
